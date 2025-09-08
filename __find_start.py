0cf97b4317858f7fd62ec602f46bfa345bdf1c96
xubarry <xubarry@outlook.com>
2025-09-08 19:12:17 +0800

    5

---
+++ __find_start.py
@@ -0,0 +1,8 @@
+﻿import sys
+path = sys.argv[1]
+sym = sys.argv[2]
+with open(path, 'r', encoding='utf-8') as f:
+    for i, line in enumerate(f, 1):
+        if line.startswith(sym):
+            print(i)
+            break
---
+++ __tmp_list.py
@@ -0,0 +1,3 @@
+﻿from chemtools.registry import search
+print('BASE sample:', len(search(role='BASE', limit=5)), search(role='BASE', limit=5))
+print('LIGAND sample:', len(search(role='LIGAND', limit=5)), search(role='LIGAND', limit=5))
